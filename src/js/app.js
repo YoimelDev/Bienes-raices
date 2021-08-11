cb9f8d10ff9ead9e52fdffa7e951cfe189d45b2c
YoimelDev <yoimeldev@gmail.com>
2021-08-10 20:16:38 -0400

    prefer dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -5,24 +5,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function darkMode() {
     const botonDarkMode = document.querySelector('.dark-mode-boton');
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', () => {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
 
     botonDarkMode.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
 
         //Para que el modo elegido se quede guardado en local-storage
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('modo-oscuro','true');
-        } else {
-            localStorage.setItem('modo-oscuro','false');
-        }
+        // if (document.body.classList.contains('dark-mode')) {
+        //     localStorage.setItem('modo-oscuro','true');
+        // } else {
+        //     localStorage.setItem('modo-oscuro','false');
+        // }
     })
 
     //Obtenemos el modo del color actual
-    if (localStorage.getItem('modo-oscuro') === 'true') {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
+    // if (localStorage.getItem('modo-oscuro') === 'true') {
+    //     document.body.classList.add('dark-mode');
+    // } else {
+    //     document.body.classList.remove('dark-mode');
+    // }
 }
 
 function eventListeners() {
