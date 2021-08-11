0ed4940b3f82a33a52f14abfebb5e553d7b69004
YoimelDev <yoimeldev@gmail.com>
2021-08-10 20:06:29 -0400

    fixed darkMode

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,21 @@ function darkMode() {
 
     botonDarkMode.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
     })
+
+    //Obtenemos el modo del color actual
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
 }
 
 function eventListeners() {
