2c5b2bc20eeae51fe5fc8b3379948999d944097b
YoimelDev <yoimeldev@gmail.com>
2021-08-12 13:36:11 -0400

    add conection with database

---
+++ includes/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+function conectarDB(): mysqli
+{
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+
+    if (!$db) {
+        echo 'Error no se puedo conectar';
+        exit;
+    }
+
+    return $db;
+}
