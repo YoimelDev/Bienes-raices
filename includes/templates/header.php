00928fb7878639e09c4b0e168f8300a27411d110
YoimelDev <yoimeldev@gmail.com>
2021-08-12 11:38:31 -0400

    add header.php

---
+++ includes/templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="build/css/app.css" />
+</head>
+
+<body>
+    <header class="header">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="./build/img/logo.svg" alt="logo de Bienes Raices" />
+                </a>
+
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="Icono menu responsive" />
+                </div>
+
+                <div class="derecha">
+                    <nav class="navegacion">
+                        <a href="nosotros.html">Nosotros</a>
+                        <a href="anuncios.html">Anuncios</a>
+                        <a href="blog.html">Blog</a>
+                        <a href="contacto.html">Contacto</a>
+                    </nav>
+
+                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="Dark mode boton" />
+                </div>
+            </div>
+            <!-- .barra -->
+        </div>
+    </header>
