a161956114bb60dfde4242db7fd19e09a483ad39
YoimelDev <yoimeldev@gmail.com>
2021-08-12 18:25:11 -0400

    uploadedFiles

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -86,19 +86,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Subida de Archivos
         // Crear carpeta
 
-        $carpetaImagenes = '../../imagenes';
+        $carpetaImagenes = '../../imagenes/';
 
         if (!is_dir($carpetaImagenes)) {
             mkdir($carpetaImagenes);
         }
 
+        // Generar un nombre unico
+        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
         // Subir la imagen
-        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . "/archivo.jpg");
+        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen);
         exit;
 
 
         // Insertar en la base de datos
-        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId')";
+        $query = "INSERT INTO propiedades (titulo, precio, imagen,descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$titulo', '$precio', '$nombreImagen','$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId')";
 
         // echo $query;
 
