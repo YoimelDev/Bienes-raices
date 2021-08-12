92d6a9b0f3e85216c720c4268ef98408a8d0e751
YoimelDev <yoimeldev@gmail.com>
2021-08-12 13:17:39 -0400

    add files for admin

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php
+require '../includes/funciones.php';
+includerTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raices</h1>
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+</main>
+
+<?php includerTemplate('footer'); ?>
