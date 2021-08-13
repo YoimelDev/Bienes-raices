53219d45fe5a92bc2b75ced2b6e09f18ee998bda
YoimelDev <yoimeldev@gmail.com>
2021-08-13 12:56:02 -0400

    delete files and propiedad

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,28 @@ $consulta = mysqli_query($db, $query);
 // Muestra mensaje condicional
 $resultado = $_GET['resultado'] ?? null;
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if ($id) {
+        // Eliminar el archivo
+        $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+        $resultado  = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
+
+        unlink('../imagenes/' . $propiedad['imagen']);
+
+        // Elimina la propiedad
+        $query = "DELETE FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            header('Location: /admin?resultado = 3');
+        }
+    }
+}
+
 // Incluye un template
 require '../includes/funciones.php';
 includerTemplate('header');
@@ -21,9 +43,11 @@ includerTemplate('header');
 <main class="contenedor seccion">
     <h1>Administrador de Bienes Raices</h1>
     <?php if ($resultado == 1) : ?>
-        <p class="alerta exito">Anuncio Areado Correctamente</p>
+        <p class="alerta exito">Anuncio Creado Correctamente</p>
     <?php elseif ($resultado == 2) : ?>
         <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+    <?php elseif ($resultado == 3) : ?>
+        <p class="alerta exito">Anuncio Eliminado Correctamente</p>
     <?php endif; ?>
 
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
@@ -49,6 +73,9 @@ includerTemplate('header');
                     <td>$ <?php echo $propiedad['precio']; ?> </td>
                     <td>
                         <form method="POST" class="w-100">
+
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
 
