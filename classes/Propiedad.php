a1d4ffcaf6ee7e811de7454ea58e2406a823650b
Yoimel Rojas <yoimeldev@gmail.com>
2021-08-20 10:31:34 -0400

    sanitize data

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -6,6 +6,7 @@ class Propiedad
 {
     // Base de datos
     protected static $db;
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
 
     public $id;
     public $titulo;
@@ -17,6 +18,12 @@ class Propiedad
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+
+    // Definir la conexion a la DB
+    public static function setDB($database)
+    {
+        self::$db = $database;
+    }
 
     public function __construct($args = [])
     {
@@ -34,6 +41,10 @@ class Propiedad
 
     public function guardar()
     {
+        // Sanitizar los datos
+        $atributos = $this->sanitizarAtributos();
+        debuguear($atributos);
+
         // Insertar en la base de datos
         $query = "INSERT INTO propiedades (titulo, precio, imagen,descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$this->titulo', '$this->precio', '$this->imagen','$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId')";
 
@@ -42,9 +53,28 @@ class Propiedad
         debuguear($resultado);
     }
 
-    // Definir la conexion a la DB
-    public static function setDB($database)
+    // Identificar y unir los atributos de la BD
+    public function atributos()
     {
-        self::$db = $database;
+        $atributos = [];
+
+        foreach (self::$columnasDB as $columna) {
+            if ($columna === 'id') continue;
+
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
+    }
+
+    public function sanitizarAtributos()
+    {
+        $atributos = $this->atributos();
+        $sanitizado = [];
+
+        foreach ($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        return $sanitizado;
     }
 }
