0b7ab389969510b9e75d93963773cb03deb7a25a
Yoimel Rojas <yoimeldev@gmail.com>
2021-08-20 07:36:00 -0400

    change conection with db

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB(): mysqli
 {
-    $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+    $db = mysqli_connect('localhost', 'root', '', 'bienes_raices');
 
     if (!$db) {
         echo 'Error no se puedo conectar';
