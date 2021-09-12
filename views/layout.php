8dc43e13c387bd5f3d3b00fcd52f57f352923508
YoimelDev <yoimeldev@gmail.com>
2021-09-12 15:03:26 -0400

    add pages initial

--- views/layout.php
+++ views/layout.php
@@ -36,12 +36,12 @@ if (!isset($inicio)) {
 
                 <div class="derecha">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros">Nosotros</a>
+                        <a href="/propiedades">Anuncios</a>
+                        <a href="/blog">Blog</a>
+                        <a href="/contacto">Contacto</a>
                         <?php if ($auth) : ?>
-                            <a href="cerrar-sesion.php">Cerrar Sesion</a>
+                            <a href="/cerrar-sesion">Cerrar Sesion</a>
                         <?php endif; ?>
                     </nav>
 
@@ -59,10 +59,10 @@ if (!isset($inicio)) {
     <footer class="footer seccion">
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
+                <a href="/nosotros">Nosotros</a>
+                <a href="/propiedades">Anuncios</a>
+                <a href="/blog">Blog</a>
+                <a href="/contacto">Contacto</a>
             </nav>
         </div>
 
