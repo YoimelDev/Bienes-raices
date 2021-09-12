26b2d7b3b0038dd6c0419963b5e53d703faf4698
YoimelDev <yoimeldev@gmail.com>
2021-09-12 13:48:28 -0400

    add validate vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -2,11 +2,33 @@
 
 namespace Controllers;
 
+use MVC\Router;
+use Model\Vendedor;
+
 class VendedorController
 {
-    public static function crear()
+    public static function crear(Router $router)
     {
-        echo 'crear';
+        $vendedor = new Vendedor;
+        $errores = Vendedor::getErrores();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Crear una nueva instancia
+            $vendedor = new Vendedor($_POST['vendedor']);
+
+            // Validar que no haya campos vacios
+            $errores = $vendedor->validar();
+
+            // No hay errores
+            if (empty($errores)) {
+                $vendedor->guardar();
+            }
+        }
+
+        $router->render('vendedores/crear', [
+            'vendedor' => $vendedor,
+            'errores' => $errores
+        ]);
     }
 
     public static function actualizar()
