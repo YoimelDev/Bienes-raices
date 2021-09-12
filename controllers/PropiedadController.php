44db98501e1e5050b94523c8fe2e2471db6ffd16
YoimelDev <yoimeldev@gmail.com>
2021-09-12 13:21:24 -0400

    add delete propiedades

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -109,4 +109,23 @@ class PropiedadController
             'errores' => $errores
         ]);
     }
+
+    public static function eliminar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Validar Id
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+
+            if ($id) {
+
+                $tipo = $_POST['tipo'];
+                if (validarTipoContenido($tipo)) {
+                    $propiedad = Propiedad::find($id);
+                    $propiedad->eliminar();
+                }
+            }
+        }
+    }
 }
