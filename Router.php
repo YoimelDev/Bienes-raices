581d4c1dcff8b91bfbf30d4bdf2ebb1ace692054
YoimelDev <yoimeldev@gmail.com>
2021-09-26 20:12:46 -0400

    add protect routes

--- Router.php
+++ Router.php
@@ -19,6 +19,20 @@ class Router
 
     public function comprobarRutas()
     {
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        // Arreglo de rutas protegidas..
+        $rutas_protegidas = [
+            '/admin',
+            '/propiedades/crear',
+            '/propiedades/actualizar',
+            '/propiedades/eliminar',
+            '/vendedores/crear',
+            '/vendedores/actualizar',
+            '/vendedores/eliminar'
+        ];
+
         $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -28,9 +42,13 @@ class Router
             $fn = $this->rutasPOST[$urlActual] ?? null;
         }
 
+        // Proteger las rutas
+        if (in_array($urlActual, $rutas_protegidas) && !$auth) {
+            header('location: /');
+        }
+
         if ($fn) {
             // La URL existe y hay una funcion asociada
-
             call_user_func($fn, $this);
         } else {
             echo "Pagina No Encontrada";
