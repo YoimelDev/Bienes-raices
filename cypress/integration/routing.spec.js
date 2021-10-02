d987fffca2fbe00d7878bc2ca054cce598906710
YoimelDev <yoimeldev@gmail.com>
2021-10-02 19:12:50 -0400

    add test routing

---
+++ cypress/integration/routing.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Prueba la navegacion y Routing del header y footer', () => {
+    it('Prueba la navegacion del header', () => {
+        cy.visit('/')
+    })
+
+    it('Prueba la navegacion del footer', () => {
+
+    })
+})
