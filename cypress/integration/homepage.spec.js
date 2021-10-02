f0a0844403f987c982dda722175ba19750400f79
YoimelDev <yoimeldev@gmail.com>
2021-10-02 11:30:14 -0400

    checking that an element exists

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -4,6 +4,8 @@ describe('Carga la pagina principal', () => {
     it('Prueba el header de la pagina principal', () => {
         cy.visit('/')
 
-        cy.get('[data-cy="heading-sitio"]')
+        cy.get('[data-cy="heading-sitio"]').should('exist')
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('equal', 'Venta de Casas y Departamentos Exclusivos de Lujo')
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('not.equal', 'Bienes Raices')
     })
 })
