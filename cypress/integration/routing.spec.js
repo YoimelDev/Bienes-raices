7ee47582ea8acab120b21e8f61ae513267c7751f
YoimelDev <yoimeldev@gmail.com>
2021-10-03 19:37:45 -0400

    testing navigation

--- cypress/integration/routing.spec.js
+++ cypress/integration/routing.spec.js
@@ -3,9 +3,43 @@
 describe('Prueba la navegacion y Routing del header y footer', () => {
     it('Prueba la navegacion del header', () => {
         cy.visit('/')
+
+        cy.get('[data-cy="navegacion-header"]').should('exist')
+        cy.get('[data-cy="navegacion-header"]').find('a').should('have.length', 4)
+        cy.get('[data-cy="navegacion-header"]').find('a').should('not.have.length', 6)
+
+        // Revisar que los enlaces sean correctos
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(0).invoke('attr', 'href').should('equal', '/nosotros')
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(0).invoke('text').should('equal', 'Nosotros')
+
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(1).invoke('attr', 'href').should('equal', '/propiedades')
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(1).invoke('text').should('equal', 'Propiedades')
+
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(2).invoke('attr', 'href').should('equal', '/blog')
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(2).invoke('text').should('equal', 'Blog')
+
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(3).invoke('attr', 'href').should('equal', '/contacto')
+        cy.get('[data-cy="navegacion-header"]').find('a').eq(3).invoke('text').should('equal', 'Contacto')
     })
 
     it('Prueba la navegacion del footer', () => {
+        cy.get('[data-cy="navegacion-footer"]').should('exist')
+        cy.get('[data-cy="navegacion-footer"]').find('a').should('have.length', 4)
+        cy.get('[data-cy="navegacion-footer"]').find('a').should('not.have.length', 6)
 
+        // Revisar que los enlaces sean correctos
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(0).invoke('attr', 'href').should('equal', '/nosotros')
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(0).invoke('text').should('equal', 'Nosotros')
+
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(1).invoke('attr', 'href').should('equal', '/propiedades')
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(1).invoke('text').should('equal', 'Propiedades')
+
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(2).invoke('attr', 'href').should('equal', '/blog')
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(2).invoke('text').should('equal', 'Blog')
+
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(3).invoke('attr', 'href').should('equal', '/contacto')
+        cy.get('[data-cy="navegacion-footer"]').find('a').eq(3).invoke('text').should('equal', 'Contacto')
+
+        cy.get('[data-cy="copyright"]').should('have.class', 'copyright')
     })
 })
--- views/layout.php
+++ views/layout.php
@@ -35,9 +35,9 @@ if (!isset($inicio)) {
                 </div>
 
                 <div class="derecha">
-                    <nav class="navegacion">
+                    <nav data-cy="navegacion-header" class="navegacion">
                         <a href="/nosotros">Nosotros</a>
-                        <a href="/propiedades">Anuncios</a>
+                        <a href="/propiedades">Propiedades</a>
                         <a href="/blog">Blog</a>
                         <a href="/contacto">Contacto</a>
                         <?php if ($auth) : ?>
@@ -58,15 +58,15 @@ if (!isset($inicio)) {
 
     <footer class="footer seccion">
         <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
+            <nav data-cy="navegacion-footer" class="navegacion">
                 <a href="/nosotros">Nosotros</a>
-                <a href="/propiedades">Anuncios</a>
+                <a href="/propiedades">Propiedades</a>
                 <a href="/blog">Blog</a>
                 <a href="/contacto">Contacto</a>
             </nav>
         </div>
 
-        <p class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy;</p>
+        <p data-cy="copyright" class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy;</p>
     </footer>
 
     <script src="/build/js/bundle.min.js"></script>
